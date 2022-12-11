34512cf0fb9ff92693717b4d78ec7c6796a5a7a9
Oksana Kaliniouk <87883684+Oksana-Kaliniouk@users.noreply.github.com>
2022-12-11 11:21:53 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+var data = {
+  "food": {
+    "title": "thing",
+    "servings": "5",
+    "ingredients": [{
+      "thing": "flour",
+      "amount": "5",
+      "measurement" : "cup"
+    }, 
+   {
+      "thing": "water",
+      "amount": "1",
+      "measurement" : "tbsp",
+     "weight": "14",
+    }, ]
+  }
+}
